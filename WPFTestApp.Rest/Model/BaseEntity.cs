c73c772e89eda87696dbed1180c23c217de0a685
Alex Rybak <shpil1993@gmail.com>
2020-08-24 17:33:23 +0300

    init

---
+++ WPFTestApp.Rest/Model/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WPFTestApp.Rest.Model
+{
+    public abstract class BaseEntity
+    {
+        [Column("ID")]
+        public int Id { get; set; }
+    }
+}
---
+++ WPFTestApp.Rest/Model/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WPFTestApp.Rest.Model
+{
+    [Table("person")]
+    public class Person : BaseEntity
+    {
+        public string Title { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Company { get; set; }
+
+        public string Street { get; set; }
+
+        public string CountryCode { get; set; }
+
+        public string Zip { get; set; }
+
+        public string City { get; set; }
+
+        public int GenderId { get; set; }
+
+        public int GreetingId { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public DateTime FirstContact { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
