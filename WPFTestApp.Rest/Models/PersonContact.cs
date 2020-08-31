27425184a2a74e82ab5aa127f77a401a31ddc44f
oleksandrRybak1993 <shpil1993@gmail.com>
2020-08-31 16:10:42 +0300

    dbContext added

---
+++ WPFTestApp.Rest/Models/PersonContact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WPFTestApp.Rest.Models
+{
+    public partial class PersonContact
+    {
+        public int PersonId { get; set; }
+        public int PersonContactId { get; set; }
+        public int ContactTypeId { get; set; }
+        public string Txt { get; set; }
+        public string Notes { get; set; }
+        public byte Active { get; set; }
+
+        public virtual Person Person { get; set; }
+    }
+}
